cdb4b2e06504286cc6f2a19bbe93c8b938c4fdcf
rfuentesgtz <70235891+rfuentesgtz@users.noreply.github.com>
2022-10-12 18:35:39 -0700

    Fixed lack of int declaration for const windowMin and windowMax

--- main/station.cpp
+++ main/station.cpp
@@ -5,8 +5,8 @@ enum mode {WAIT, DIFS, BACKOFF, SENDING, SIFS, ACK};
 class station {
 public:
 	int windowSize = 4;
-	const windowMin = 4;
-	const windowMax = 1024;
+	const int windowMin = 4;
+	const int windowMax = 1024;
 
 	int successCount = 0;
 	int collisionCount = 0;
